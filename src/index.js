33a20722dce6d521e04c29da33b8072f6357571f
Fabio Caprara <fats140701@gmail.com>
2022-08-08 12:20:37 -0400

    Estrutura Inicial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./assets/reset.css";
+import "./assets/style.css";
+
+const root = document.querySelector(".root");
+ReactDOM.render(<App />, root);
